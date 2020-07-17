1d7c0a90cd84120614e7ff0d64dda2abb464b277
Olivia Cubela <oliviacubela@gmail.com>
2020-07-17 04:23:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_reader :side_one, :side_two, :side_three
+  attr_accessor :side_one, :side_two, :side_three
 
   def initialize(side_one:, side_two:, side_three:)
     @side_one = side_one
