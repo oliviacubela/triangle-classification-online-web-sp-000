1f15118a17bb0869808772ef24f610222e6dfac8
Olivia Cubela <oliviacubela@gmail.com>
2020-07-17 04:22:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,25 @@ class Triangle
     @side_two = side_two
     @side_three = side_three
   end
+
+  def kind
+    if (@side_one * @side_two * @side_three) == 0 || (@side_one + @side_two) <= @side_three || (@side_two + @side_three) <= @side_one || (@side_one + @side_three) <= @side_two
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    elsif @side_one == @side_two && @side_one == @side_three 
+      self.kind == :equilateral
+    elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
+      self.kind == :isosceles
+    else 
+      self.kind == :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
+  
 end
