f9ad8014cd12a27d99371014513fe48defdfabaa
Olivia Cubela <oliviacubela@gmail.com>
2020-07-17 04:27:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,8 @@ class Triangle
       end
     elsif side_one == side_two && side_one == side_three
       self.kind == :equilateral
-    elsif side_one == side_two || side_one == side_three || side_two == side_three
+    elsif
+      side_one == side_two || side_one == side_three || side_two == side_three
       self.kind == :isosceles
     else
       self.kind == :scalene
