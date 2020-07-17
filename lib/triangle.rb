a6f229c13e2aa895d4b80381e04d544ff079cbd7
Olivia Cubela <oliviacubela@gmail.com>
2020-07-17 04:33:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,13 @@ class Triangle
         raise TriangleError
         puts error.message
       end
-    elsif
-      side_one == side_two && side_one == side_three
+    elsif side_one == side_two && side_one == side_three 
       self.kind == :equilateral
+
     elsif
       side_one == side_two || side_one == side_three || side_two == side_three
       self.kind == :isosceles
+
     else
       self.kind == :scalene
     end
